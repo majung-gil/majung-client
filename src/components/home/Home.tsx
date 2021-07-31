4d327a43ce6d62fbe1fe16028d8aaf8881f35e10
2ujin <suddenlydisappear200@gmail.com>
2021-07-31 14:35:35 +0900

    feat: 카카오 지도 api 추가

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,27 @@
+/* eslint import/newline-after-import: "off" */
+import { useEffect } from 'react';
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 function Home() {
-  return <div>home</div>;
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    const map = new window.kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="map" style={{ width: '100vw', height: '100vh' }} />
+    </div>
+  );
 }
 
 export default Home;
